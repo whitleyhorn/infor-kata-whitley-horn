28a344f5165400f08bb3fcfe8c7d9713923d6a1d
Whitley Horn <whorn@corp.lawyer.com>
2022-01-07 11:16:16 -0500

    Replace <style> tag in templates with Constructable Stylesheets

--- js/custom-button.js
+++ js/custom-button.js
@@ -1,29 +1,32 @@
 // Insert Button JS
 const buttonTemplate = document.createElement('template');
 buttonTemplate.innerHTML = `
-	<style>
-        button {
-            background-color: #B7B7BA;
-            border: 1px solid #B7B7BA;
-            color: #47474C;
-            min-height: 34px;
-            padding: 0 30px;
-            font-weight: bold;
-            margin-left: 20px;
-            cursor: pointer;
-        }
-        button:hover {
-            background: #9f9fa1;
-        }
-	</style>
 	<button class="custom-button">
 	</button>
 `;
+
+const buttonSheet = new CSSStyleSheet();
+buttonSheet.replaceSync(`
+    button {
+        background-color: #B7B7BA;
+        border: 1px solid #B7B7BA;
+        color: #47474C;
+        min-height: 34px;
+        padding: 0 30px;
+        font-weight: bold;
+        margin-left: 20px;
+        cursor: pointer;
+    }
+    button:hover {
+        background: #9f9fa1;
+    }
+`);
 
 class CustomButton extends HTMLElement {
 	constructor(){
 		super();
 		this.attachShadow({ mode: 'open'});
+        this.shadowRoot.adoptedStyleSheets = [buttonSheet];
 		this.shadowRoot.appendChild(buttonTemplate.content.cloneNode(true));
 	}
 
--- js/custom-message-dialog.js
+++ js/custom-message-dialog.js
@@ -1,58 +1,6 @@
 // insert message js
 const dialogTemplate = document.createElement('template');
 dialogTemplate.innerHTML = `
-	<style>
-        .wrapper {
-            align-items: center;
-            display: flex;
-            flex-direction: column;
-            height: 100%;
-            justify-content: center;
-            margin: 0 auto;
-            position: absolute;
-            width: 100%;
-        }
-
-        .modal {
-           width: 427px; 
-        }
-
-        .content {
-            background: #ffffff;
-            border-radius: 2px;
-            box-shadow: 0 2px 5px rgb(0 0 0 / 20%);
-            color: #606066;
-            margin: 16px 16px 0;
-            position: relative;
-            padding: 16px;
-        }
-
-        .content .header {
-            font-size: 28px;
-        }
-
-        .content .header img {
-            height: 20px;
-        }
-
-        .content .body {
-            margin: 32px 0px 30px;
-        }
-
-        .button {
-            background: white;
-            margin: 0 16px;
-            text-align: center;
-            color: #0066d4;
-            border-top: 1px solid black;
-            padding: 16px;
-            cursor: pointer;
-        }
-
-        .hide {
-            display: none;
-        }
-	</style>
     <div class="wrapper hide">
        <div class="modal">
          <div class="content">
@@ -70,10 +18,65 @@ dialogTemplate.innerHTML = `
     </div>
 `;
 
+const dialogSheet = new CSSStyleSheet();
+dialogSheet.replaceSync(`
+    .wrapper {
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        justify-content: center;
+        margin: 0 auto;
+        position: absolute;
+        width: 100%;
+    }
+
+    .modal {
+       width: 427px; 
+    }
+
+    .content {
+        background: #ffffff;
+        border-radius: 2px;
+        box-shadow: 0 2px 5px rgb(0 0 0 / 20%);
+        color: #606066;
+        margin: 16px 16px 0;
+        position: relative;
+        padding: 16px;
+    }
+
+    .content .header {
+        font-size: 28px;
+    }
+
+    .content .header img {
+        height: 20px;
+    }
+
+    .content .body {
+        margin: 32px 0px 30px;
+    }
+
+    .button {
+        background: white;
+        margin: 0 16px;
+        text-align: center;
+        color: #0066d4;
+        border-top: 1px solid black;
+        padding: 16px;
+        cursor: pointer;
+    }
+
+    .hide {
+        display: none;
+    }
+`);
+
 class CustomMessageDialog extends HTMLElement {
 	constructor(){
 		super();
 		this.attachShadow({ mode: 'open'});
+        this.shadowRoot.adoptedStyleSheets = [dialogSheet];
 		this.shadowRoot.appendChild(dialogTemplate.content.cloneNode(true));
 	}
     
