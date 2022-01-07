47183084483025f2f205170eb60d9db47d0a9716
Whitley Horn <whorn@corp.lawyer.com>
2022-01-06 23:32:57 -0500

    Finish project

--- js/custom-button.js
+++ js/custom-button.js
@@ -1,1 +1,42 @@
 // Insert Button JS
+const buttonTemplate = document.createElement('template');
+buttonTemplate.innerHTML = `
+	<style>
+        button {
+            background-color: #B7B7BA;
+            border: 1px solid #B7B7BA;
+            color: #47474C;
+            min-height: 34px;
+            padding: 0 30px;
+            font-weight: bold;
+            margin-left: 20px;
+            cursor: pointer;
+        }
+        button:hover {
+            background: #9f9fa1;
+        }
+	</style>
+	<button class="custom-button">
+	</button>
+`;
+
+class CustomButton extends HTMLElement {
+	constructor(){
+		super();
+		this.attachShadow({ mode: 'open'});
+		this.shadowRoot.appendChild(buttonTemplate.content.cloneNode(true));
+        this.button.innerText = this.getAttribute('text');
+	}
+
+    get button(){
+        return this.shadowRoot.querySelector('button');
+    }
+
+    connectedCallback(){
+        const clickEvent = new CustomEvent('clicked');
+        const dispatch = this.dispatchEvent.bind(this, clickEvent);
+        this.button.addEventListener('click', dispatch);
+    }
+}
+
+window.customElements.define('custom-button', CustomButton);
--- js/custom-message-dialog.js
+++ js/custom-message-dialog.js
@@ -1,1 +1,100 @@
 // insert message js
+const dialogTemplate = document.createElement('template');
+dialogTemplate.innerHTML = `
+	<style>
+        .wrapper {
+            align-items: center;
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+            justify-content: center;
+            margin: 0 auto;
+            position: absolute;
+            width: 100%;
+        }
+
+        .modal {
+           width: 427px; 
+        }
+
+        .content {
+            background: #ffffff;
+            border-radius: 2px;
+            box-shadow: 0 2px 5px rgb(0 0 0 / 20%);
+            color: #606066;
+            margin: 16px 16px 0;
+            position: relative;
+            padding: 16px;
+        }
+
+        .content .header {
+            font-size: 28px;
+        }
+
+        .content .header img {
+            height: 20px;
+        }
+
+        .content .body {
+            margin: 32px 0px 30px;
+        }
+
+        .button {
+            background: white;
+            margin: 0 16px;
+            text-align: center;
+            color: #0066d4;
+            border-top: 1px solid black;
+            padding: 16px;
+            cursor: pointer;
+        }
+
+        .hide {
+            display: none;
+        }
+	</style>
+    <div class="wrapper hide">
+       <div class="modal">
+         <div class="content">
+            <div class="header">
+                <img src="images/error-alert.svg">
+                <span>Lost Connection</span>
+            </div>
+            <div class="body">
+                <div>This application has experienced a <b>system error</b> due to the lack of internet access.</div>
+                <div><i>Please</i> restart the application in order to proceed.</div>
+            </div>
+         </div>
+        <div class="button">Restart Now</div>
+       </div> 
+    </div>
+`;
+
+class CustomMessageDialog extends HTMLElement {
+	constructor(){
+		super();
+		this.attachShadow({ mode: 'open'});
+		this.shadowRoot.appendChild(dialogTemplate.content.cloneNode(true));
+	}
+    
+    get wrapper(){
+        // First div selected will be outer div, always
+        return this.shadowRoot.querySelector('div.wrapper');
+    }
+
+    show(){
+        this.wrapper.classList.remove('hide');
+        document.querySelector('.overlay').classList.remove('hide');
+    }
+
+    hide(){
+        this.wrapper.classList.add('hide');
+        document.querySelector('.overlay').classList.add('hide');
+    }
+
+    connectedCallback(){
+        this.shadowRoot.querySelector('div.button').addEventListener('click', this.hide.bind(this));
+    }
+}
+
+window.customElements.define('custom-message-dialog', CustomMessageDialog);
