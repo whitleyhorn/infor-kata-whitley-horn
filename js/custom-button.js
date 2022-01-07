21e5148e61b9a5278fb999e07ddd8389c173abf1
Whitley Horn <whorn@corp.lawyer.com>
2022-01-07 12:23:16 -0500

    Clean up CSS to match example given

--- js/custom-button.js
+++ js/custom-button.js
@@ -8,14 +8,17 @@ buttonTemplate.innerHTML = `
 const buttonSheet = new CSSStyleSheet();
 buttonSheet.replaceSync(`
     button {
+        font-size: 16px;
         background-color: #B7B7BA;
         border: 1px solid #B7B7BA;
         color: #47474C;
         min-height: 34px;
         padding: 0 30px;
-        font-weight: bold;
+        font-weight: 600;
         margin-left: 20px;
+        margin: 20px 0 0 20px;
         cursor: pointer;
+        font-family: "source sans pro", helvetica, arial, sans-serif;
     }
     button:hover {
         background: #9f9fa1;
--- js/custom-message-dialog.js
+++ js/custom-message-dialog.js
@@ -2,8 +2,7 @@
 const dialogTemplate = document.createElement('template');
 dialogTemplate.innerHTML = `
     <div class="wrapper hide">
-       <div class="modal">
-         <div class="content">
+       <div class="modalContent">
             <div class="header">
                 <img src="images/error-alert.svg">
                 <span>Lost Connection</span>
@@ -12,8 +11,7 @@ dialogTemplate.innerHTML = `
                 <div>This application has experienced a <b>system error</b> due to the lack of internet access.</div>
                 <div><i>Please</i> restart the application in order to proceed.</div>
             </div>
-         </div>
-        <div class="button">Restart Now</div>
+            <div class="button">Restart Now</div>
        </div> 
     </div>
 `;
@@ -32,40 +30,43 @@ dialogSheet.replaceSync(`
         top: 0;
     }
 
-    .modal {
-       width: 427px; 
-    }
-
-    .content {
+    .modalContent {
+        width: 401px; 
+        height: 220px;
         background: #ffffff;
+        border: 1px solid #B7B7BA;
         border-radius: 2px;
         box-shadow: 0 2px 5px rgb(0 0 0 / 20%);
         color: #606066;
-        margin: 16px 16px 0;
+        margin: 10px;
         position: relative;
-        padding: 16px;
     }
 
-    .content .header {
-        font-size: 28px;
+    .header {
+        font-size: 26px;
+        padding: 16px 16px 0;
     }
 
-    .content .header img {
+    .header span {
+        margin-left: 10px;
+    }
+
+    .header img {
         height: 20px;
     }
 
-    .content .body {
-        margin: 32px 0px 30px;
+    .body {
+        margin: 32px 16px;
     }
 
     .button {
         background: white;
-        margin: 0 16px;
         text-align: center;
         color: #0066d4;
-        border-top: 1px solid black;
+        border-top: 1px solid #B7B7BA;
         padding: 16px;
         cursor: pointer;
+        font-weight: 600;
     }
 
     .hide {
